d6bf5e4d185d68f3f4bde1680f85c224ff09503a
DarshanVaishya <DarshanVaishya3@gmail.com>
2022-02-03 16:23:49 +0530

    commit #6

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,6 @@
 const path = require("path");
 
 module.exports = {
-	// What mode to use? development or production
-	// production code is minified
-	mode: "development",
-	devtool: "source-map",
 	// Location of entry file or main file
 	entry: "./src/index.js",
 	output: {
@@ -12,5 +8,22 @@ module.exports = {
 		filename: "main.js",
 		// Path to the folder where file will be generated
 		path: path.resolve(__dirname, "dist"),
+	},
+
+	// What mode to use? development or production
+	// production code is minified
+	mode: "development",
+	devtool: "source-map",
+
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				// css-loader takes the css styles and puts them into main.js
+				// style-loader takes the styles and injects them into the dom
+				// The order of execution is reverse. First css-loader is executed then style-loader.
+				use: ["style-loader", "css-loader"],
+			},
+		],
 	},
 };
